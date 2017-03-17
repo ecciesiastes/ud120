1e4cfdedcf8856b581b5479d1b05012184d5a5c2
leishu <leishu@sina.com>
2017-03-17 14:28:35 +0800

    k_means

--- k_means/k_mean_cluster2.py
+++ k_means/k_mean_cluster2.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 
-""" 
+"""
     Skeleton code for k-means clustering mini-project.
 """
 
@@ -8,6 +8,11 @@ import pickle
 import numpy
 import matplotlib.pyplot as plt
 import sys
+
+
+##向特征列表（features_list）中添加第三个特征：“total_payments”。
+
+
 
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
@@ -35,23 +40,41 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 
 ### load in the dict of dicts containing all the data on each person in the dataset
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
-### there's an outlier--remove it! 
+### there's an outlier--remove it!
 data_dict.pop("TOTAL", 0)
 
+
+
+
+def maxmin(item):
+    min = sys.float_info[0]
+    max = 0
+    for data in data_dict:
+        value = data_dict[data][item]
+        if value != 'Nan':
+            num = float(value)
+            if num < min:
+                min = num
+            if num > max:
+                max = num
+    return max, min
+
+
 ### the input features we want to use
-### can be any key in the person-level dictionary (salary, director_fees, etc.) 
+### can be any key in the person-level dictionary (salary, director_fees, etc.)
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list)
 poi, finance_features = targetFeatureSplit(data)
 
-### in the "clustering with 3 features" part of the mini-project,
-### you'll want to change this line to 
+### in the "clustering with 4 features" part of the mini-project,
+### you'll want to change this line to
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, f3 in finance_features:
     plt.scatter(f1, f2)
 plt.show()
 
@@ -67,8 +90,10 @@ pred = clf.predict(finance_features)
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters2.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print("no predictions object named pred found, no clusters to plot")
 
 
+print(maxmin('exercised_stock_options'))
+print(maxmin('salary'))
