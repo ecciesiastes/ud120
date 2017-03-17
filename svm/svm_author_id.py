dc7e7580235f01ec929569f646927e762b04e7c9
leishu <leishu@sina.com>
2017-03-17 10:29:12 +0800

    svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,8 +19,26 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 
 
+from sklearn.svm import SVC
+clf = SVC(kernel="rbf", C=10000)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+num = 0
+for p in pred:
+    if p == 1:
+        num+=1
+
+print(num)
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+print(acc)
 
 #########################################################
 ### your code goes here ###
